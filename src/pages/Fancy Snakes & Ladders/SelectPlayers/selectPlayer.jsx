f3b92a9919ab1e5d883a1431748985e47e0f0b64
phANTom2303 <anikavy.goenka@gmail.com>
2024-08-14 14:03:05 +0530

    Implemented a Number of players selector component

---
+++ src/pages/Fancy Snakes & Ladders/SelectPlayers/selectPlayer.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import styles from './selectPlayer.module.css'
+function SelectPlayers({ onSubmit }) {
+    const [players, setPlayers] = useState(2);
+
+    function decreasePlayer(){
+        if(players == 2)
+            setPlayers(6);
+        else
+            setPlayers(players - 1);
+    }
+
+    function increasePlayer(){
+        if(players == 6)
+            setPlayers(2);
+        else
+            setPlayers(players + 1);
+    }
+
+    return (
+        <div className={styles.selectPlayer}>
+            <p> Select Number of Players :</p>
+            <div className="playerCounter">
+                <button onClick={() => decreasePlayer()}> -1 </button>
+                <div>{players}</div>
+                <button onClick={() => increasePlayer()}>+1</button>
+            </div>
+            <button onClick={() => onSubmit(players)}>Submit</button>
+        </div>
+    );
+}
+export default SelectPlayers;
--- src/pages/Fancy Snakes & Ladders/fancySL.jsx
+++ src/pages/Fancy Snakes & Ladders/fancySL.jsx
@@ -1,6 +1,20 @@
+import { useState } from "react";
+import SelectPlayers from "./SelectPlayers/selectPlayer";
+import './fancySL.css';
 function FancySL() {
-    return (
-        <div>Fancy SL</div>
-    );
+    const [playerCount, setPlayerCount] = useState(0);
+
+    if (playerCount != 0) {
+        return (
+            <div className="fancySL">Player Count = {playerCount}</div>
+        );
+    }
+    else {
+        return (
+            <div className="fancySL">
+            <SelectPlayers onSubmit= {setPlayerCount}></SelectPlayers>
+            </div>
+        );
+    }
 }
 export default FancySL
