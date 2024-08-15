8f0ad42d89aea3d2d5e18b62622f0ec8f3f1a43d
phANTom2303 <anikavy.goenka@gmail.com>
2024-08-15 21:52:03 +0530

    Added a game board img

--- src/pages/Fancy Snakes & Ladders/fancySL.jsx
+++ src/pages/Fancy Snakes & Ladders/fancySL.jsx
@@ -44,7 +44,7 @@ function FancySL() {
 
     function movePlayer(player, moves) {
         const newPos = playerPostions;
-        newPos[player] = newPos[player] +moves;
+        newPos[player] = newPos[player] + moves;
         updatePlayerPositions(newPos);
     }
 
@@ -63,7 +63,7 @@ function FancySL() {
             updateCurrentPlayer(0);
     }
 
-    function resetGame(){
+    function resetGame() {
         updateMessage("Waiting for Dice Roll...");
         updateCurrentPlayer(0);
         updatePlayerPositions(Array(6).fill(null));
@@ -85,126 +85,129 @@ function FancySL() {
                     <button onClick={() => gameLoop()}> Roll Dice </button>
                 </div>
                 <div>{message}</div>
-                <div className="row">
-                    <FancySLTile tileNumber={99} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={98} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={97} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={96} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={95} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={94} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={93} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={92} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={91} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={90} playerPos={playerPostions} />
+                <div className="slboard">
+                    <div className="row">
+                        <FancySLTile tileNumber={99} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={98} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={97} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={96} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={95} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={94} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={93} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={92} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={91} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={90} playerPos={playerPostions} />
+                    </div>
+                    <div className="row">
+                        <FancySLTile tileNumber={80} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={81} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={82} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={83} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={84} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={85} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={86} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={87} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={88} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={89} playerPos={playerPostions} />
+                    </div>
+                    <div className="row">
+                        <FancySLTile tileNumber={79} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={78} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={77} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={76} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={75} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={74} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={73} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={72} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={71} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={70} playerPos={playerPostions} />
+                    </div>
+                    <div className="row">
+                        <FancySLTile tileNumber={60} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={61} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={62} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={63} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={64} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={65} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={66} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={67} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={68} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={69} playerPos={playerPostions} />
+                    </div>
+                    <div className="row">
+                        <FancySLTile tileNumber={59} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={58} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={57} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={56} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={55} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={54} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={53} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={52} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={51} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={50} playerPos={playerPostions} />
+                    </div>
+                    <div className="row">
+                        <FancySLTile tileNumber={40} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={41} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={42} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={43} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={44} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={45} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={46} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={47} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={48} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={49} playerPos={playerPostions} />
+                    </div>
+                    <div className="row">
+                        <FancySLTile tileNumber={39} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={38} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={37} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={36} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={35} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={34} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={33} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={32} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={31} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={30} playerPos={playerPostions} />
+                    </div>
+                    <div className="row">
+                        <FancySLTile tileNumber={20} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={21} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={22} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={23} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={24} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={25} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={26} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={27} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={28} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={29} playerPos={playerPostions} />
+                    </div>
+                    <div className="row">
+                        <FancySLTile tileNumber={19} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={18} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={17} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={16} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={15} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={14} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={13} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={12} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={11} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={10} playerPos={playerPostions} />
+                    </div>
+                    <div className="row">
+                        <FancySLTile tileNumber={0} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={1} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={2} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={3} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={4} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={5} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={6} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={7} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={8} playerPos={playerPostions} />
+                        <FancySLTile tileNumber={9} playerPos={playerPostions} />
+                    </div>
                 </div>
-                <div className="row">
-                    <FancySLTile tileNumber={80} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={81} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={82} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={83} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={84} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={85} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={86} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={87} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={88} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={89} playerPos={playerPostions} />
-                </div>
-                <div className="row">
-                    <FancySLTile tileNumber={79} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={78} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={77} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={76} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={75} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={74} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={73} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={72} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={71} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={70} playerPos={playerPostions} />
-                </div>
-                <div className="row">
-                    <FancySLTile tileNumber={60} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={61} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={62} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={63} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={64} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={65} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={66} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={67} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={68} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={69} playerPos={playerPostions} />
-                </div>
-                <div className="row">
-                    <FancySLTile tileNumber={59} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={58} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={57} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={56} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={55} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={54} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={53} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={52} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={51} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={50} playerPos={playerPostions} />
-                </div>
-                <div className="row">
-                    <FancySLTile tileNumber={40} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={41} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={42} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={43} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={44} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={45} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={46} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={47} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={48} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={49} playerPos={playerPostions} />
-                </div>
-                <div className="row">
-                    <FancySLTile tileNumber={39} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={38} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={37} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={36} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={35} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={34} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={33} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={32} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={31} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={30} playerPos={playerPostions} />
-                </div>
-                <div className="row">
-                    <FancySLTile tileNumber={20} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={21} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={22} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={23} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={24} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={25} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={26} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={27} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={28} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={29} playerPos={playerPostions} />
-                </div>
-                <div className="row">
-                    <FancySLTile tileNumber={19} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={18} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={17} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={16} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={15} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={14} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={13} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={12} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={11} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={10} playerPos={playerPostions} />
-                </div>
-                <div className="row">
-                    <FancySLTile tileNumber={0} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={1} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={2} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={3} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={4} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={5} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={6} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={7} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={8} playerPos={playerPostions} />
-                    <FancySLTile tileNumber={9} playerPos={playerPostions} />
-                </div>
+
                 <button onClick={() => resetGame()}>Reset</button>
             </div>
         );
