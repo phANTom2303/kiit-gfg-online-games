e2f47251a4deff2b4a3bf455bffded05c8831863
VanXodus305 <soumyadeep.30505@gmail.com>
2024-10-21 09:47:46 +0530

    Minor CSS tweaks

--- src/pages/hangman/Main.jsx
+++ src/pages/hangman/Main.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { Footer } from './Footer';
-import { ContentGraphics } from './MiddlePart';
-import { randomWord } from './RandomWords';
+import React, { useState, useEffect } from "react";
+import { Footer } from "./Footer";
+import { ContentGraphics } from "./MiddlePart";
+import { randomWord } from "./RandomWords";
 
 function Main() {
   const [noOfWrong, setNoOfWrong] = useState(0);
-  const [answer, setAnswer] = useState('');
-  const [category, setCategory] = useState('');
+  const [answer, setAnswer] = useState("");
+  const [category, setCategory] = useState("");
 
   const reset = () => {
     setNoOfWrong(0);
@@ -15,21 +15,20 @@ function Main() {
       setAnswer(newWord.word);
       setCategory(newWord.category);
     } catch (error) {
-      console.error('Error resetting game:', error);
+      console.error("Error resetting game:", error);
     }
   };
 
   useEffect(() => {
-    reset(); 
-  }, []); 
+    reset();
+  }, []);
 
   const updateNoOfWrong = (newCount) => {
     setNoOfWrong(newCount);
   };
 
   return (
-    <div className="flex flex-col h-full">
-      
+    <div className="flex flex-col h-full px-6 mx-auto">
       <main className="flex-grow flex flex-col sm:flex-row">
         <ContentGraphics
           noOfWrong={noOfWrong}
